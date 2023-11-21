9f5fe5fd7ae9689b89e6e7ed2c45f47a02c8f1ec
sanjeevWeb <108945506+sanjeevWeb@users.noreply.github.com>
2023-11-21 13:08:14 +0530

    updtated database with sequalize

---
+++ first_add_to _mysql/database_sequalize.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('kumardb', 'root', 'root123', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
+
+module.exports = sequelize;
